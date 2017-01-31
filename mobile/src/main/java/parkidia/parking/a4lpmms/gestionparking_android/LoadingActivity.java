bfdb3e342a42f9fce7137b726e049f7b9a0c3d61
matthieubravo <matthieubravo12@gmail.com>
2017-01-31 13:49:10 +0100

    création du layout de chargement

--- mobile/src/main/java/parkidia/parking/a4lpmms/gestionparking_android/LoadingActivity.java
+++ mobile/src/main/java/parkidia/parking/a4lpmms/gestionparking_android/LoadingActivity.java
@@ -66,7 +66,7 @@ public class LoadingActivity extends AppCompatActivity {
     }
 
 
-    /////////////////////////////////
+    //////////////////////////////////
 
     /**
      * Cacher les barres d'interface androïd
